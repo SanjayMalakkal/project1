8e10638cdc45b9732914b1d7c3575491da36a174
sanjay <sanjaymalakkal222@gmail.com>
2022-07-01 10:40:01 +0530

    new version2

--- sampleproject2/settings.py
+++ sampleproject2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d=#r*wug$-m5(*=ji1x78wf&@(&8*+$$$1k69_1slh5-amcu2i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
